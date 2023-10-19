082fa530b9dc16efcc25da7c23485bbb06c429ec
kanghung <lokanghung@allpower.in>
2023-10-19 16:41:16 +0800

    edit sql file

--- restaurant_reviews.sql
+++ restaurant_reviews.sql
@@ -73,7 +73,7 @@ CREATE TABLE `reviews` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `reviews`
+-- Dumping data for table `reviews` 
 --
 
 LOCK TABLES `reviews` WRITE;
